3825032c3924b00ae7bc98cafd2e4eeeb06b71b3
zubayer-salehin <freelancerzubear@gmail.com>
2022-11-24 12:07:12 +0600

    database url change

--- Utils/dbConnect.js
+++ Utils/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function DBConnect() {
-    mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.5pmu7.mongodb.net/job-portal?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log(`Mongoess Connection Successfully`);
         })
